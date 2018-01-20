de537e42dd07c44c28a417bfa549d751303f78fb
katies62381 <2021SjobK75@moundsviewschools.org>
2018-01-19 18:30:51 -0600

    Pseudo Elevator Code

--- src/main/java/frc/team2052/powerup/Constants.java
+++ src/main/java/frc/team2052/powerup/Constants.java
@@ -4,4 +4,7 @@ package frc.team2052.powerup;
  * Created by KnightKrawler on 1/15/2018.
  */
 public class Constants {
+    public static int inchmultiplier = 12
+            public static int
+
 }
---
+++ src/main/java/frc/team2052/powerup/subsystems/Intake.java
@@ -0,0 +1,7 @@
+package frc.team2052.powerup.subsystems;
+
+/**
+ * Created by Kay on 1/19/2018.
+ */
+public class Intake {
+}
