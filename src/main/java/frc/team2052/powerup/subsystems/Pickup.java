0be6220820b3b87eec854a2e324efe9b006b2d84
KatieS <2021SjobK75@moundsviewschools.org>
2018-02-03 13:09:09 -0600

    Added positions for intake up/down mechanism to go to

--- src/main/java/frc/team2052/powerup/subsystems/Pickup.java
+++ src/main/java/frc/team2052/powerup/subsystems/Pickup.java
@@ -11,11 +11,16 @@ public class Pickup {
     public static Pickup getInstance() {return instance;}
 
     private Solenoid inOutSolonoid, upDownSolonoid;
+    private Solenoid upDownSolonoidLIn, upDownSolonoidLOut;
+    private Solenoid upDownSolonoidSIn, upDownSolonoidSOut;
     private TalonSRX leftMotor, rightMotor;
 
     private Pickup() {
         inOutSolonoid = new Solenoid(PickupConstants.inOutSolenoid);
-        upDownSolonoid = new Solenoid(PickupConstants.upDownSolenoid);
+        upDownSolonoidLIn = new Solenoid(PickupConstants.upDownLSolenoidIn);
+        upDownSolonoidLOut = new Solenoid(PickupConstants.upDownSolenoid1Out);
+        upDownSolonoidSIn = new Solenoid(PickupConstants.upDownSolenoid2In);
+        upDownSolonoidSOut = new Solenoid(PickupConstants.upDownSolenoid2Out);
         leftMotor = new TalonSRX(PickupConstants.pickupLeftMotorId);
         rightMotor = new TalonSRX(PickupConstants.pickupRightMotorId);
         rightMotor.setInverted(true);
@@ -35,22 +40,35 @@ public class Pickup {
     }
 
     public void intake(){
-        setMotorSpeed(0.5);
+        setMotorSpeed(0.6);
     }
 
     public void outtake() {
-        setMotorSpeed(0.5);
+        setMotorSpeed(-0.4);
     }
 
     public void stopped() {
         setMotorSpeed(0);
     }
 
-    public void up() {
-        upDownSolonoid.set(true);
+    public void position1() {
+        upDownSolonoidLIn.set(true);
+        upDownSolonoidLOut.set(false);
+        upDownSolonoidSIn.set(true);
+        upDownSolonoidSOut.set(false);
     }
 
-    public void down() {
-        upDownSolonoid.set(false);
+    public void position2() {
+        upDownSolonoidLIn.set(true);
+        upDownSolonoidLOut.set(false);
+        upDownSolonoidSIn.set(false);
+        upDownSolonoidSOut.set(true);
+    }
+
+    public void position3() {
+        upDownSolonoidLIn.set(false);
+        upDownSolonoidLOut.set(true);
+        upDownSolonoidSIn.set(false);
+        upDownSolonoidSOut.set(true);
     }
 }
