6523458b9578995bf0207ea13bde08909ce680da
Scott Davis <github2@scott-davis.com>
2018-02-13 20:41:37 -0600

    Copied code from Chief Delphi to talk to Pixy Cam

---
+++ src/main/java/frc/team2052/powerup/subsystems/vision/PixyException.java
@@ -0,0 +1,7 @@
+package frc.team2052.powerup.subsystems.vision;
+
+public class PixyException extends Exception {
+    public PixyException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/frc/team2052/powerup/subsystems/vision/PixyPacket.java
@@ -0,0 +1,8 @@
+package frc.team2052.powerup.subsystems.vision;
+
+public class PixyPacket {
+    public int X;
+    public int Y;
+    public int Width;
+    public int Height;
+}
