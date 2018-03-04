6cc51477983ee8f9814d4bc99b91a28314210f66
Scott Davis <github2@scott-davis.com>
2018-03-04 13:10:02 -0600

    Added simple auto test to see if "inches per second" acutally go inches per second

---
+++ src/main/java/frc/team2052/powerup/auto/modes/TestVelocity.java
@@ -0,0 +1,22 @@
+package frc.team2052.powerup.auto.modes;
+
+import com.first.team2052.lib.path.Path;
+import com.first.team2052.lib.vec.Translation2d;
+import frc.team2052.powerup.auto.AutoMode;
+import frc.team2052.powerup.auto.AutoModeEndedException;
+import frc.team2052.powerup.auto.AutoModeSelector;
+import frc.team2052.powerup.auto.AutoPaths;
+import frc.team2052.powerup.auto.actions.*;
+
+import java.util.Arrays;
+
+public class TestVelocity extends AutoMode {
+
+    @Override
+    protected  void init() throws AutoModeEndedException {
+        System.out.println("RUNNING VELOCITY PATH TEST");
+        runAction(new SeriesAction(Arrays.asList(
+            new DriveVelocityHeadingAction(10,5)
+        )));
+    }
+}
