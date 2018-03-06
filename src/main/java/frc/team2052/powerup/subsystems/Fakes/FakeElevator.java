06c48b733e5bb3a7dd6d3211e69a5c6967d7a5e0
KnightKralwer <controls@team2052.com>
2018-03-06 16:14:49 -0600

    Added print lines for our fake elevator, so we know when we are using the fake one.

--- src/main/java/frc/team2052/powerup/subsystems/Fakes/FakeElevator.java
+++ src/main/java/frc/team2052/powerup/subsystems/Fakes/FakeElevator.java
@@ -13,37 +13,40 @@ public class FakeElevator implements ElevatorSubsystem, Loopable {
     private double currentInches = 0;
     @Override
     public void zeroSensor() {
-
+        System.out.println("FAKE ELEVATOR: Zeroing fake sensor");
     }
 
     @Override
     public double getHeightInches() {
+        System.out.println("FAKE ELEVATOR: Returning 'Height'");
         return currentInches;
     }
 
 
     @Override
     public void setTarget(Elevator.ElevatorPresetEnum posEnum) {
+        System.out.println("FAKE ELEVATOR: Setting Fake Target");
         currentInches = getHeightInchesForPreset(posEnum);
     }
 
     @Override
     public void setCurrentPosAsTarget() {
-
+        System.out.println("FAKE ELEVATOR: Setting Current Pos As Fake Target");
     }
 
     @Override
     public void setEmergencyHold(boolean isPressed) {
-
+        System.out.println("FAKE ELEVATOR: Setting the fake to emergency stop");
     }
 
     @Override
     public void setEmergencyUp(boolean isPressed) {
-
+        System.out.println("FAKE ELEVATOR: Setting the fake to emergency up");
     }
 
     @Override
     public boolean getCarriageIsMoving() {
+        System.out.println("FAKE ELEVATOR: Carriage isn't moving");
         return false;
     }
 
@@ -61,14 +64,19 @@ public class FakeElevator implements ElevatorSubsystem, Loopable {
     public int getHeightInchesForPreset(Elevator.ElevatorPresetEnum posEnum){
         switch (posEnum){
             case PICKUP:
+                System.out.println("FAKE ELEVATOR: Picking up nothing");
                 return Constants.kElevatorMinHeight;
             case SWITCH:
+                System.out.println("FAKE ELEVATOR: Moving nothing to switch height");
                 return Constants.kElevatorSwitchHeight;
             case SCALE_BALANCED:
+                System.out.println("FAKE ELEVATOR: Moving nothing to normal scale height");
                 return (int)(Constants.kElevatorMaxHeight * .6);
             case SCALE_HIGH:
+                System.out.println("FAKE ELEVATOR: Moving nothing to tall scale height");
                 return (int)(Constants.kElevatorMaxHeight * .8);
             case SCALE_HIGH_STACKING:
+                System.out.println("FAKE ELEVATOR: Moving nothing to ExtraTall scale height");
                 return Constants.kElevatorMaxHeight;
         }
         return 0;
