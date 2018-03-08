94e6e00596f9220dac1bcf7a2900b533a78af391
Ian Olds <ian.olds@team2052.com>
2018-03-07 22:34:11 -0600

    added test intake

--- src/main/java/frc/team2052/powerup/auto/AutoMode.java
+++ src/main/java/frc/team2052/powerup/auto/AutoMode.java
@@ -267,7 +267,7 @@ public abstract class AutoMode extends AutoModeBase {
                 new ParallelAction(Arrays.asList(
                         new TimeoutAction(new FollowPathAction(new Path(AutoPaths.LLScale), false), 10), //pathing to the left scale
                         new SeriesAction(Arrays.asList(new WaitForPathMarkerAction("RaiseElevator"),
-                                new  ElevatorAction(Elevator.ElevatorPresetEnum.SCALE_HIGH_STACKING), //Elevator raises to place on balanced scale
+                                new ElevatorAction(Elevator.ElevatorPresetEnum.SCALE_HIGH_STACKING), //Elevator raises to place on balanced scale
                                 new MoveArmAction(MoveArmAction.ArmPositionEnum.DOWN))))), //lowers pickup to position pointing out
                 new PickupAction(PickupAction.PickupStateEnum.TIMEDOUTTAKE) //pushes cube out
         );
