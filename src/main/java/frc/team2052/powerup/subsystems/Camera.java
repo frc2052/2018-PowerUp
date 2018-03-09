5f2c09ae67560440dfa50cfb068c86212e3165fa
Ian Olds <ian.olds@team2052.com>
2018-03-09 12:33:55 -0600

    driver cam with shuffle

--- src/main/java/frc/team2052/powerup/subsystems/Camera.java
+++ src/main/java/frc/team2052/powerup/subsystems/Camera.java
@@ -1,11 +1,7 @@
 package frc.team2052.powerup.subsystems;
 
 import edu.wpi.cscore.AxisCamera;
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.CameraServer;
-import org.opencv.core.Mat;
 
 public class Camera {
     private static Camera instance = null;
@@ -32,8 +28,9 @@ public class Camera {
 //            AxisCamera camera = CameraServer.getInstance().addAxisCamera("10.20.52.11");
             AxisCamera camera = CameraServer.getInstance().addAxisCamera("axis-camera.local");
             //SmartDashboard.putStringArray("/CameraPublisher/AxisCamera/streams", axisArray);
-            NetworkTableInstance.getDefault().getEntry("/CameraPublisher/AxisCamera/streams").setStringArray(axisArray);
+           // NetworkTableInstance.getDefault().getEntry("/CameraPublisher/AxisCamera/streams").setStringArray(axisArray);
             camera.setResolution(320, 240);
+            /*
             CvSink cvSink = CameraServer.getInstance().getVideo();
             CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 320, 240);
             Mat mat = new Mat();
@@ -45,6 +42,7 @@ public class Camera {
                 }
                 outputStream.putFrame(mat);
             }
+            */
         });
         visionThread.setDaemon(true);
         visionThread.start();
